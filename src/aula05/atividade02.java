effc9ca5e198de4c380f5c07b786be226cc7cdf5
Maria Eduarda Vasconcelos <eduuardavasconcelos@gmail.com>
2023-12-12 16:21:59 -0300

    atualizando atividade aula 05

--- src/aula05/atividade02.java
+++ src/aula05/atividade02.java
@@ -10,17 +10,15 @@ public class atividade02 {
 
 		Scanner leia = new Scanner(System.in);
 		int numero;
+		
 		ArrayList<Integer> numeros = new ArrayList<Integer>();
 
 		for (int i = 1; i <= 10; i++) {
 			numeros.add(i * 10);
 		}
-
 		
 		System.out.println("Digite um número: ");
 		numero = leia.nextInt();
-		
-		
 		
 		if (numeros.indexOf(numero)>=0) {
 		System.out.println("O número " + numero + " está localizado " + "na posição: " + numeros.indexOf(numero));
